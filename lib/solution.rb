79be3c0c29796d1acc6f5ee42d8f303b4457a6e2
github username <github email address>
2019-12-30 09:37:57 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,16 @@
 def clock_angle(time)
-    # code your solution here
+    numbers = time.split(":")
+    hour = numbers[0].to_f
+    minutes = numbers[1].to_f
+
+    hour = hour%12
+
+    hour_hand_degree = (hour * 60 + minutes) * 0.5
+    minute_hand_degree = 6 * minutes
+
+    diff = hour_hand_degree - minute_hand_degree
+    diff *= -1 if diff < 0
+    diff = 360 - diff if diff > 180
+
+    diff
 end
